ac08beba1ff46347cff782ba9d3741bc97c40599
Owoo-Lay <jeansnflykicks@gmail.com>
2021-06-23 17:50:31 +0100

    lets commit

--- app/src/main/java/com/nwanneka/todoapp/HomeActivity.kt
+++ app/src/main/java/com/nwanneka/todoapp/HomeActivity.kt
@@ -2,15 +2,18 @@ package com.nwanneka.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var usernameDisplay: TextView
+    private lateinit var profileButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        profileButton = findViewById(R.id.profileButton)
 
         usernameDisplay = findViewById(R.id.usernameDisplay)
 
---
+++ app/src/main/java/com/nwanneka/todoapp/ProfileActivity.kt
@@ -0,0 +1,17 @@
+package com.nwanneka.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var homeButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        homeButton = findViewById(R.id.homeButton)
+    }
+}
